b809d350d71577fb4c4bbedee4537b94ccfc9b77
zhafranth <zhafranth@gmail.com>
2026-04-06 20:16:27 +0700

    feat: register service worker via vite-plugin-pwa

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,7 @@
+/// <reference types="vite-plugin-pwa/client" />
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './index.css'
 
@@ -8,3 +10,5 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+registerSW({ immediate: true })
